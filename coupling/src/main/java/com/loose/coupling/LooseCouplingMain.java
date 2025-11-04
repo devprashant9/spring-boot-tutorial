d690f10b890ce645c07ab9f37d2fa868e14e5aac
devprashant <kumarjha183@gmail.com>
2025-11-04 11:46:52 +0530

    important terminologies

--- coupling/src/main/java/com/loose/coupling/LooseCouplingMain.java
+++ coupling/src/main/java/com/loose/coupling/LooseCouplingMain.java
@@ -9,5 +9,25 @@ public class LooseCouplingMain {
         UserDataProvider newService = new NewService();
         UserManager newServiceWithDB = new UserManager(newService);
         System.out.println(newServiceWithDB.getUserInfo());
+
+        // Loose Coupling
+        // Future Integration Requires Minimal or No Code Changes
+
+        // Inversion of Control
+        // Object is Created and Managed by Developer. Frameworks needs to manage it
+
+        // Dependency Injection
+        // UserManager needs an instance of UserDataProvider
+
+        // Beans
+        // Objects Managed by Framework From Config. Files. Eg, Line 5 and Line 6
+
+        // Spring Container
+        // Place Where all Objects to be Used at Run-Time Resides
+
+        // Config File
+        // A File Used to Instruct Spring Container Which Object to be Used When
+
+
     }
 }
