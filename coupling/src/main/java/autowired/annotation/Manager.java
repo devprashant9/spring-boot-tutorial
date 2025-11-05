963d819651af22e462d539ae9cd19b66a839bc1c
devprashant <kumarjha183@gmail.com>
2025-11-05 10:36:07 +0530

    @Qualifier annotation use case

--- coupling/src/main/java/autowired/annotation/Manager.java
+++ coupling/src/main/java/autowired/annotation/Manager.java
@@ -1,11 +1,13 @@
 package autowired.annotation;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component("manager")
 public class Manager {
     @Autowired
+    @Qualifier("employee")
     private Employee emp;  // field injection
 
 //    @Autowired
