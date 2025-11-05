cd311f76299c32a14d4f6794c9405e56cb74be14
devprashant <kumarjha183@gmail.com>
2025-11-05 10:08:56 +0530

    @Value annotation

--- coupling/src/main/java/components/configuration/Employee.java
+++ coupling/src/main/java/components/configuration/Employee.java
@@ -1,12 +1,20 @@
 package components.configuration;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component ("employee")
 public class Employee {
+    @Value("123")
     private int employeeID;
+
+    @Value("Prashant ${java.home}")
     private String firstName;
+
+    @Value("Kumar #{4 * 4}")
     private String lastName;
+
+    @Value("100000")
     private double salary;
 
     public int getEmployeeID() {
