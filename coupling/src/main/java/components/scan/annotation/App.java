d4cf82eabc1163fc3d4758a26e262caf9ef70736
devprashant <kumarjha183@gmail.com>
2025-11-05 10:25:21 +0530

    moving from xm to class config

---
+++ coupling/src/main/java/components/scan/annotation/App.java
@@ -0,0 +1,12 @@
+package components.scan.annotation;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class App {
+    public static void main(String[] args) {
+        ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+        Employee emp = (Employee) context.getBean("employee");
+        System.out.println(emp.toString());
+    }
+}
---
+++ coupling/src/main/java/components/scan/annotation/AppConfig.java
@@ -0,0 +1,9 @@
+package components.scan.annotation;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "components.scan.annotation")
+public class AppConfig {
+}
