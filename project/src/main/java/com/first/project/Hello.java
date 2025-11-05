880f149e1360d756e407eb5cd342c0c4295f4009
devprashant <kumarjha183@gmail.com>
2025-11-05 11:34:11 +0530

    POST request & dynamic param

--- project/src/main/java/com/first/project/Hello.java
+++ project/src/main/java/com/first/project/Hello.java
@@ -1,6 +1,8 @@
 package com.first.project;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,5 +11,15 @@ public class Hello {
     @GetMapping("/hello")
     public String hello() {
         return "Hello World";
+    }
+
+    @GetMapping("/name/{id}")
+    public String userId(String id) {
+        return id;
+    }
+
+    @PostMapping("/hello")
+    public String helloPost(@RequestBody String name) {
+        return "Hello " + name + " !";
     }
 }
